9848b5ec3b85557c5d0b37fe6593e9123e08711a
Geetha Chegonda <geetha.chegonda@zf.com>
2025-11-20 13:39:40 +0530

    worked on backend to save server using autosave

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,39 +1,47 @@
 import express from "express";
 
 const articleInfo = [
-  { name: "learn-node", upvotes: 0 },
-  { name: "learn-react", upvotes: 0 },
-  { name: "mongodb", upvotes: 0 },
+  { name: "learn-node", upvotes: 0, comments: [] },
+  { name: "learn-react", upvotes: 0, comments: [] },
+  { name: "mongodb", upvotes: 0, comments: [] },
 ];
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/hello", function (req, res) {
-  res.send("Hello from a GET endpoint !");
-});
-
-app.get("/hello/:name", function (req, res) {
-  res.send("Hello, " + req.params.name);
-});
-
-app.post("/hello", function (req, res) {
-  res.send("Hello, " + req.body.name + " from POST endpoint!");
-});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
   const article = articleInfo.find((a) => a.name === req.params.name);
   article.upvotes += 1;
 
-  res.send(
-    "Success! The article " +
-      req.params.name +
-      " now has " +
-      article.upvotes +
-      " upvotes!"
-  );
+  res.json(article);
 });
 
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find((a) => a.name === name);
+
+  article.comments.push({
+    postedBy,
+    text,
+  });
+
+  res.json(article);
+});
 app.listen(8000, function () {
   console.log("Server is listening on port 8000");
 });
+
+// app.get("/hello", function (req, res) {
+//   res.send("Hello from a GET endpoint !");
+// });
+
+// app.get("/hello/:name", function (req, res) {
+//   res.send("Hello, " + req.params.name);
+// });
+
+// app.post("/hello", function (req, res) {
+//   res.send("Hello, " + req.body.name + " from POST endpoint!");
+// });
